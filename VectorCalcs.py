f389e0268674623bc1444840ea5996982c83714c
George Kounatidis <57770693+KGL-Gr@users.noreply.github.com>
2021-07-23 17:47:11 +0300

    Better calcs
    The only problem is that the angle has to be determined as clockwise or counterclockwise.

--- VectorCalcs.py
+++ VectorCalcs.py
@@ -15,6 +15,22 @@ for i in range(10):
 print(XPoints)
 print(YPoints)
 
+#1st angle starts from 0(0,0) to a random point
+if XPoints[1]!=0 :
+    Angle = np.round( np.arctan(YPoints[1]/XPoints[1]),2)
+    if YPoints[1]>0:
+        AngleArray.append(np.round((Angle*180)/np.pi +np.pi, 2))
+    else:
+        AngleArray.append(np.round((Angle * 180) / np.pi - np.pi, 2))
+else:
+    if YPoints[1]>0:
+        AngleArray.append(90)
+    elif YPoints[1]<0:
+        AngleArray.append(-90)
+    else:
+        print("Error!")
+
+
 for i in range(1, 10):
     Norm = np.round(np.sqrt(np.power(XPoints[i] - XPoints[i - 1], 2) + np.power(YPoints[i] - YPoints[i - 1], 2)), 2)
     NormArray.append(Norm)
@@ -22,13 +38,19 @@ for i in range(1, 10):
 for i in range(1, 9):
 
     #Needs 3 norms to form a triangle: OAB -> ABC -> BCD -> CDE -> ...
-    if NormArray[i] == 0:
-        Angle = 0
+    ## To avoid Big Numbers as args in arccosine
+    if Growth_Ratio >= 1000:
+        Angle = 90
+        continue
+    elif Growth_Ratio <= -1000:
+        Angle = -90
+        continue
     else:
         # Al-Kashi's theorem Law of Cosines.
 
-        Triangle_Completion = np.round(np.sqrt( np.power(XPoints[i+1] - XPoints[i-1], 2) + np.power(YPoints[i+1] - YPoints[i-1], 2)))
-        Angle = np.arccos(( np.power(NormArray[i], 2) + np.power(NormArray[i-1], 2) - np.power(Triangle_Completion, 2)) / (2*NormArray[i]*NormArray[i-1]))
+        Triangle_Completion = np.round(np.sqrt( np.power((XPoints[i+1] - XPoints[i-1]), 2) + np.power(YPoints[i+1] - YPoints[i-1], 2)), 2)        
+        Arcosine_Arg = round((np.power(NormArray[i], 2) + np.power(NormArray[i-1], 2) - np.power(Triangle_Completion, 2)) /(2*NormArray[i]*NormArray[i-1]),2)
+        Angle = np.arccos(Arcosine_Arg)
 
     AngleArray.append( np.round((Angle*180)/(np.pi),2)*-1 +180)
 print(NormArray)
