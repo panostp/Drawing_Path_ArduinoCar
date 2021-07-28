a2a4aa72f26437fd7b92d0a0d12eeaccd2b6d75e
George Kounatidis <57770693+KGL-Gr@users.noreply.github.com>
2021-07-28 16:39:37 +0300

    Filter out copies

---
+++ CopiesReduce.py
@@ -0,0 +1,14 @@
+A = [0, 0, 1, 10, 4, 4, 5, 6, 6, 6, 6, 6, 9, 9, 1, 1, 5, 3, 2, 2, 1, 1, 1, 3, 5, 5, 4, 3]
+B = []
+j = 0
+i = 0
+B.append(A[i])
+
+# Used with round(x, 2).
+
+while i < A.__len__()-1:
+    i += 1
+    if A[i] == A[i-1]:
+        continue
+    B.append(A[i])
+print(B)
