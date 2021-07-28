cb51efd3445faa25aaf5ccd7b4c9b4fc85b866d3
George Kounatidis <57770693+KGL-Gr@users.noreply.github.com>
2021-07-28 21:03:55 +0300

    Update CopiesReduce.py

--- CopiesReduce.py
+++ CopiesReduce.py
@@ -1,6 +1,6 @@
 import numpy as np
-A = [0, 0, 1, 10, 4, 4, 5, 6, 6, 6, 6, 6, 9, 9, 1, 1, 5, 3, 2, 2, 1, 1, 1, 3, 5, 5, 4, 3]   # X values
-C = [0, 0, 1, 10, 4, 4, 5, 6, 6, 7, 7, 7, 9, 9, 1, 0, 3, 3, 4, 5, 6, 1, 1, 3, 2, 1, 1, 0]   # Y values
+A = [0, 0.3, 0.5, 0.6, 0.6, 0.6, 0.7, 0.9, 1, 1, 1.1, 1, 1, 1, 1.1, 1, 1.1, 1, 0.9, 0.9, 0.89, 0.88, 0.89, 0.9, 1, 1]   # X values
+C = [0, 0.3, 0.5, 0.5, 0.5, 0.3, 0.4, 0.4, 0.6, 0.7, 1.1, 1, 1, 1, 1, 1, 1.1, 1.2, 1.1, 1, 0.99, 0.98, 0.99, 1, 1, 1]   # Y values
 B = []  # New X values
 D = []  # New Y Values
 NormArray = []  # Norms
@@ -16,11 +16,21 @@ for i in range(1, A.__len__()):   # Find the distance between samples
     Norm = np.round(np.sqrt(np.power(A[i] - A[i - 1], 2) + np.power(C[i] - C[i - 1], 2)), 2)
     NormArray.append(Norm)
 
+# Padding for last Values
+k = A.__len__()-1
+tempV = A[k]
+A.append(tempV)
+tempV = C[k]
+C.append(tempV)
+NormArray.append(0)
+
+    
 B.append(A[0])
 D.append(C[0])
 
 for i in range(1, A.__len__()-1):
-    if (NormArray[i] <= 0.5) | (A[i] == A[i-1] & C[i] == C[i-1]):     # Isn't 100% true, angles have to be taken into consideration. Not so difficult..
+    if (NormArray[i] <= 0.01 and NormArray[i-1] <= 0.01) or (A[i] == A[i-1] and C[i] == C[i-1]):    
+        # Isn't 100% true, angles have to be taken into consideration. Not so difficult..
         continue                                                    # Angles matter more than Norms, here.
     # Really small angles are the ones that should be filtered out.
     # Also, Norms that are like <0.1 and have angles close to 0 should be filtered out too.
@@ -35,3 +45,4 @@ plt.show()
 print(NewNorm)
 print(B)
 print(D)
+print(B.__len__())
