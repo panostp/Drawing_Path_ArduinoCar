9b8b870e711e2d2f28cef211db179868818db2cb
panostp <63464224+panostp@users.noreply.github.com>
2021-08-19 14:42:57 +0300

    Add files via upload

---
+++ flutter/lib/controllerapp.dart
@@ -0,0 +1,149 @@
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'chatpage.dart';
+
+// to BluetoothDevice server deixnei to device name kai alla stoixia gia connection
+//opote ka8e file pou xreiazetai gia sundesh to 8elei
+//ama 8es na to deis bgale thn class controllerapp kai allakse to class ths
+// "class ControllerAppMain extends State<ControllerApp>"----->"class ControllerApp extends StatefulWidget"
+//kai meta phgaine sto main kai ekei pou exei to sfalma bgale tis metablhtes se ControllerApp()
+class ControllerApp extends StatefulWidget {
+  final BluetoothDevice server;
+
+  const ControllerApp({required this.server});
+
+  @override
+  ControllerAppMain createState() => new ControllerAppMain();
+}
+
+class ControllerAppMain extends State<ControllerApp> {
+  @override
+  Widget build(BuildContext context) {
+    Widget outside = new Container(
+      //gamo thn flutter
+      width: 200.0,
+      height: 200.0,
+      decoration: BoxDecoration(
+        color: Colors.black38,
+        shape: BoxShape.circle,
+      ),
+    );
+    Widget inside = new Container(
+      width: 60.0,
+      height: 60.0,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        shape: BoxShape.circle,
+      ),
+    );
+    return new Material(
+      color: Colors.white,
+      child: Center(
+          child: Stack(children: <Widget>[
+        outside,
+        Positioned(
+          child: IconButton(
+              onPressed: movement("1"),
+              icon: const Icon(Icons.arrow_drop_up)), //pano
+          top: 10,
+          left: 80,
+        ),
+        Positioned(
+          child: IconButton(
+              onPressed: movement("2"),
+              icon: const Icon(Icons.arrow_drop_down)), //kato
+          bottom: 10,
+          left: 80,
+        ),
+        Positioned(
+          child: IconButton(
+              onPressed: movement("3"),
+              icon: const Icon(Icons.arrow_left_sharp)), //aristera
+          top: 80,
+          left: 10,
+        ),
+        Positioned(
+          child: IconButton(
+              onPressed: movement("4"),
+              icon: const Icon(Icons.arrow_right)), //deksia
+          top: 80,
+          right: 10,
+        ),
+        Positioned(
+          child: inside,
+          top: 70,
+          left: 70,
+        )
+      ])),
+    );
+  }
+
+  //sends
+  void sendMessage(String movement) async {
+    BluetoothConnection? connection;
+    final TextEditingController textEditingController =
+        new TextEditingController();
+    textEditingController.clear();
+
+    if (movement.length > 0) {
+      try {
+        connection!.output.add(Uint8List.fromList(utf8.encode(movement)));
+        await connection.output.allSent;
+      } catch (e) {
+        //notify state
+        setState(() {});
+      }
+    }
+  }
+
+  //movement den ksero akoma ama doulebei sosta 8elei tests
+  movement(var mov) {
+    switch (mov) {
+      case 1:
+        () => sendMessage("F");
+        break;
+      case 2:
+        () => sendMessage("B");
+        break;
+      case 3:
+        () => sendMessage("L");
+        break;
+      case 4:
+        () => sendMessage("R");
+        break;
+      default:
+        () => sendMessage("Stop");
+    }
+  }
+}
+
+class cirlce extends StatelessWidget {
+  final GestureTapCallback onTap;
+  final IconData iconData;
+
+  const cirlce({Key? key, required this.onTap, required this.iconData})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double size = 50.0;
+
+    return new InkResponse(
+      onTap: onTap,
+      child: new Container(
+        width: size,
+        height: size,
+        decoration: new BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+        ),
+        child: new Icon(
+          iconData,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter/lib/draw.dart
@@ -0,0 +1,243 @@
+//import 'dart:ffi';
+import 'dart:ui';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'dart:typed_data';
+import 'dart:convert';
+
+//otan bazeis px: int? something; dhloneis pos to something mporei na einai nullable
+//required shmainei pos h metablhth prepei na mhn einai null
+//to slider den doulebei kala
+class Draw extends StatefulWidget {
+  final BluetoothDevice server;
+
+  const Draw({required this.server});
+
+  @override
+  DrawMain createState() => new DrawMain();
+}
+
+//class Widthpaint {
+//  Offset cords;
+//  Paint stroke;
+
+// Widthpaint({required this.cords, required this.stroke});
+//}
+
+class DrawMain extends State<Draw> {
+  List<Offset> cords = [];
+  late double strokeWidth;
+  @override
+  void initState() {
+    super.initState();
+    strokeWidth = 1.0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width; //kanto kai sto gamepad
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Stack(
+        children: <Widget>[
+          Center(
+              child: Column(
+            //oste o pinakas na einai kentro
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                      width: width * 0.9,
+                      height: height * 0.9,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                blurRadius: 5,
+                                spreadRadius: 1)
+                          ]),
+                      child: GestureDetector(
+                        onPanDown: (details) {
+                          this.setState(() {
+                            cords.add(details.localPosition);
+                          });
+                        },
+                        onPanUpdate: (details) {
+                          this.setState(() {
+                            this.setState(() {
+                              cords.add(details.localPosition);
+                            });
+                          });
+                        },
+                        onPanEnd: (details) {
+                          this.setState(() {
+                            // cords.add(details);
+                            print("mesa end");
+                          });
+                        },
+                        child: SizedBox.expand(
+                            child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          child: CustomPaint(
+                            painter: Painte(cords: cords),
+                          ),
+                        )),
+                      ))),
+              //container gia to slider isos to sbhso giati apla ta gama alla 8a krathso to cleaner
+              Container(
+                width: width * 0.80,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Slider(
+                        min: 1.0,
+                        max: 10.0,
+                        label: "Stroke $strokeWidth",
+                        value: strokeWidth,
+                        onChanged: (double value) {
+                          this.setState(() {
+                            strokeWidth = value;
+                          });
+                        },
+                      ),
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.cleaning_services,
+                          color: Colors.black,
+                        ),
+                        onPressed: () {
+                          this.setState(() {
+                            cords.clear();
+                          });
+                        }),
+                  ],
+                ),
+              )
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+int forwardCounter = 0;
+
+//drawing class
+class Painte extends CustomPainter {
+  List<Offset> cords;
+  Painte({required this.cords});
+  double lastangle = -1.5707963267948966;
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint background = Paint()..color = Colors.white;
+    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.drawRect(rect, background);
+    canvas.clipRect(rect);
+    Paint paint = Paint();
+    paint.color = Colors.black54;
+    paint.strokeWidth = 10.0;
+    paint.isAntiAlias = true;
+    paint.strokeCap = StrokeCap.round;
+    //logika kapou edo einai to bug
+    //x,y
+    int s = 0;
+    int j = 100;
+    for (int i = 0; i < cords.length - 1; i++) {
+      if (cords[i] != null && cords[i + 1] != null) {
+        canvas.drawLine(cords[i], cords[i + 1], paint);
+        Offset line1 = cords[i];
+        Offset line2 = cords[i + 1];
+        if (j % 100 == 0) {
+          // prinsome();
+          offseTodouble(line1, line2);
+        }
+        j++;
+      } else if (cords[i] != null && cords[i + 1] == null) {
+        print("fuck");
+        canvas.drawPoints(PointMode.points, [cords[i]], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(Painte oldDelegate) {
+    return true;
+  }
+
+//line1
+  double disY1 = 1;
+  double disX1 = 1;
+//line2
+  double disY2 = 1;
+  double disX2 = 1;
+
+  void offseTodouble(line1, line2) {
+    //Offset distance = line1 + line2;
+    disY1 = line2.dy - line1.dy;
+    disX1 = line2.dx - line1.dx;
+    turn();
+  }
+
+  double get distance => sqrt(disX1 * disX2 + disY1 * disY2);
+  //double get direction => atan2(disY1, disX1);
+
+  double get angle => atan2(disY1, disX1);
+
+  void prinsome() {
+    //print(distance);
+    print(angle);
+  }
+
+  void turn() {
+    double currentAngle = angle;
+    double currentDistance = distance;
+
+    if (lastangle != currentAngle) {
+      if (currentAngle < lastangle) {
+        print("turn left!");
+        forwardCounter = 0;
+        () => sendMessage("L");
+      } else if (currentAngle > lastangle) {
+        forwardCounter = 0;
+        print("turn right!");
+        () => sendMessage("R");
+      }
+    } else {
+      forwardCounter++;
+      print(forwardCounter);
+    }
+    lastangle = currentAngle;
+    if (forwardCounter > 2) {
+      //send command go forward
+      () => sendMessage("F");
+      forwardCounter = 0;
+    }
+  }
+}
+
+void sendMessage(String movement) async {
+  BluetoothConnection? connection;
+  final TextEditingController textEditingController =
+      new TextEditingController();
+  textEditingController.clear();
+
+  if (movement.length > 0) {
+    try {
+      connection!.output.add(Uint8List.fromList(utf8.encode(movement)));
+      await connection.output.allSent;
+    } catch (e) {
+      //notify state
+      //setState(() {});
+    }
+  }
+}
